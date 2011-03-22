990d0e318d5710ebdbf279a73fa28c7e72781c17
TJBeta <TJBeta@gmail.com>
2011-03-22 17:17:35 -0400

    Added php code

---
+++ order.php
@@ -0,0 +1,24 @@
+<?php
+
+$fName = $_POST["fName"];
+$lName = $_POST["lName"];
+$phoneNum = $_POST["phone"];
+$address = $_POST["address"];
+$city = $_POST["city"];
+$state = $_POST["state"];
+$zip = $_POST["zip"];
+$cName = $_POST["cName"];
+$cNumber = $_POST["cNumber"];
+$cvm = $_POST["cvm"];
+$exMonth = $_POST["exMonth"];
+$exYear = $_POST["exYear"];
+$pType = $_POST["pizzaType"];
+$pSize = $_POST["pizzaSize"];
+// Figure this out...
+$pToppings = $_POST["toppings"];
+
+// Set up a confirmation
+
+// Set up email
+
+// Set up google maps
